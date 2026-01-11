2a48db5665a512f351eb6512481673cfb8bf5374
cristiak24 <baramarius23@stud.ase.ro>
2026-01-11 02:40:19 +0200

    Fix port render si rutahome

--- frontend-react/src/config.js
+++ frontend-react/src/config.js
@@ -1,2 +1,7 @@
-const isLocal = window.location.hostname === 'localhost';
-export const API_BASE_URL = isLocal ? 'http://localhost:3000' : 'https://proiect-tehnologii-web-bug-tracking.onrender.com';
+// src/config.js
+
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+export const API_BASE_URL = isLocal 
+    ? 'http://localhost:3000' 
+    : 'https://proiect-tehnologii-web-bug-tracking.onrender.com';
